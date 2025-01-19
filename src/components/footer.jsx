d41cfd86ecb56d847ad2cc07af2b67b0c95ee3b5
Kevin Deal <kevin@julyfivedesign.com>
2025-01-19 00:00:40 -0500

    merge complete

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="relative footer-overlay-component">
+    <footer className="relative footer-overlay-component overflow-hidden">
       <div className="footer-line"></div>
       <div className="footer-content text-center py-6">
         <p className="date">Hire Kevin Deal Frontend Software Engineer {currentYear}</p>
